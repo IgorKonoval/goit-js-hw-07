ac22328e802ab98a2a25823853a7f9e53665bc6c
Igor Konoval <kip_y@sfa.org.ua>
2023-09-02 22:08:16 +0300

    end 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const container = document.querySelector(".gallery");
-const markup = galleryItems.map(
-  ({ preview, original, description }) =>
-    `<li class="gallery__item">
-      <a class="gallery__link" href="${original}" target="_top" rel="noopener noreferrer">
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
@@ -13,24 +15,36 @@ const markup = galleryItems.map(
         />
       </a>
     </li>`
-);
+  )
+  .join("");
 
-container.insertAdjacentHTML("beforeend", markup.join(""));
+container.insertAdjacentHTML("beforeend", markup);
 container.addEventListener("click", onClick);
 
-function onClick(params) {
-  if (!params.target.classList.contains(".gallery__item")) {
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__item")) {
     return;
   }
 
-  const instance = basicLightbox.create(`
-   <img
-      src="${original}"
-      alt="${description}"
-    />
-`);
-  console.log(instance);
-  instance.show();
-}
+  const imgSource = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${imgSource}" width="1280" height="auto">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", escKeyPress);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", escKeyPress);
+      },
+    }
+  );
 
-console.log(galleryItems);
+  instance.show();
+
+  function escKeyPress(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
