2fe82d653383225a5fed8dd786daf798d7cb489a
Igor Konoval <kip_y@sfa.org.ua>
2023-09-02 15:09:04 +0300

    edit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const container = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}" target="_top" rel="noopener noreferrer">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+);
+
+container.insertAdjacentHTML("beforeend", markup.join(""));
+container.addEventListener("click", onClick);
+
+function onClick(params) {
+  if (!params.target.classList.contains(".gallery__item")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+   <img
+      src="${original}"
+      alt="${description}"
+    />
+`);
+  console.log(instance);
+  instance.show();
+}
 
 console.log(galleryItems);
